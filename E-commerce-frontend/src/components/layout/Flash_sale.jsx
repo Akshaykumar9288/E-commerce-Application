e880c2b58ad4f8183542f7e5b741819792469ee9
Akshay Kumar <akshayk0552@gmail.com>
2026-07-05 17:27:57 +0530

    Intreact with backend application

--- E-commerce-frontend/src/components/layout/Flash_sale.jsx
+++ E-commerce-frontend/src/components/layout/Flash_sale.jsx
@@ -5,59 +5,72 @@ import product2 from "../../assets/images/product2.jpg";
 import product3 from "../../assets/images/product3.jpg";
 import product4 from "../../assets/images/product4.jpg";
 import { useEffect, useState } from "react";
+import axios from "axios";
+// import env from "react-dotenv";
 function FlashSale() {
-    const products=[
+    const products = [
         {
-            id:1,
+            id: 1,
             img: product1,
             name: "SoundMax Pro Wireless",
             price: "$129.00",
             originalPrice: "$329.00",
             discount: "80% OFF"
         },
-            {
-                id:2,
-                img: product2,
-                name: "Velocity Watch Series 4",
-                price: "$199.00",
-                originalPrice: "$350.00",
-                discount: "45% OFF"
+        {
+            id: 2,
+            img: product2,
+            name: "Velocity Watch Series 4",
+            price: "$199.00",
+            originalPrice: "$350.00",
+            discount: "45% OFF"
         },
-            {
-                id:3,
-                img: product3,
-                name: "Lumix X5 Mirrorless",
-                price: "$899.00",
-                originalPrice: "$1200.00",
-                discount: "30% OFF"
+        {
+            id: 3,
+            img: product3,
+            name: "Lumix X5 Mirrorless",
+            price: "$899.00",
+            originalPrice: "$1200.00",
+            discount: "30% OFF"
         },
-            {
-                id:4,
-                img: product4,
-                name: "Nitro Run Performance",
-                price: "$75.00",
-                originalPrice: "$150.00",
-                discount: "50% OFF"
+        {
+            id: 4,
+            img: product4,
+            name: "Nitro Run Performance",
+            price: "$75.00",
+            originalPrice: "$150.00",
+            discount: "50% OFF"
         },
 
         {
-                id:4,
-                img: product4,
-                name: "Nitro Run Performance",
-                price: "$75.00",
-                originalPrice: "$150.00",
-                discount: "50% OFF"
+            id: 5,
+            img: product4,
+            name: "Nitro Run Performance",
+            price: "$75.00",
+            originalPrice: "$150.00",
+            discount: "50% OFF"
         }
     ]
-     const [time, setTime] = useState(new Date());
+    const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
 
-    return () => clearInterval(timer); // Cleanup
-  }, []);
+        return () => clearInterval(timer); // Cleanup
+    }, []);
+    const [post, setPost] = useState([]);
+
+useEffect(() => {
+    console.log(import.meta.env);
+    axios.get(import.meta.env.VITE_FLASH_SALE_API_URL)
+        .then((response) => {
+            console.log(response.data);
+            setPost(response.data.flashsale);
+        })
+        .catch((err) => console.log(err));
+}, []);
     return (
         <section className="Flash_sale w-full h-[541px] py-[48px] bg-[rgba(255,220,189,0.44)]">
             <div className="flash-sale-container w-full h-full px-[24px] gap-[31px] flex flex-col">
@@ -76,24 +89,47 @@ function FlashSale() {
                     </div>
                 </div>
                 <div className="product-card-section pb-[24px] h-full w-full flex items-center gap-[24px] overflow-hidden">
-                    {products.map((product)=>(
-                    <div key={product.id} className="product-card px-[16px] py-[16px] min-w-[280px] h-full flex flex-col gap-[4px] items-start bg-[#fff] rounded-[12px] ">
-                        <div className="product-image h-[192px] w-full flex justify-center items-center rounded-[8px] bg-[#F8F9FA] relative" style={{ backgroundImage: `url(${product.img})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                            <div className="percent-off absolute top-[8px] left-[8px] bg-[#BA1A1A] w-[61px] h-[23px] rounded-[4px] py-[4px] px-[8px] text-white text-[10px] font-bold leading-[15px]"><p>{product.discount}</p></div>
-                        </div>
-                        <div className="product-name w-full h-[36px] pt-[12px]">
-                            <p className="text-[#191C1D] leading-[24px]">{product.name}</p>
-                        </div>
-                        <div className="product-price h-[24px] w-full flex gap-[8px] items-center">
-                            <p className="actual-price text-[#8A5100] font-bold leading-[24px]">{product.price}</p>
-                            <p className="actual-price text-[#554434] text-sm  leading-[24px] line-through">{product.originalPrice}</p>
-                        </div>
-                        <div className="add-to-card-button pt-[20px] pb-[8px] h-[52px] w-full flex justify-center items-center">
-                            <a href="#!" className="text-[#0061A3] text-center leading-[24px]">Add to Cart</a>
-                        </div>
-                    </div>
-                    ))}
-                    
+                    {post.map((product) => (
+    <div
+        key={product._id}
+        className="product-card px-[16px] py-[16px] min-w-[280px] h-full flex flex-col gap-[4px] items-start bg-[#fff] rounded-[12px]"
+    >
+        <div
+            className="product-image h-[192px] w-full flex justify-center items-center rounded-[8px] bg-[#F8F9FA] relative"
+            style={{
+                backgroundImage: `url(${product.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+            }}
+        >
+            <div className="percent-off absolute top-[8px] left-[8px] bg-[#BA1A1A] w-[61px] h-[23px] rounded-[4px] py-[4px] px-[8px] text-white text-[10px] font-bold leading-[15px]">
+                <p>{product.discount}% OFF</p>
+            </div>
+        </div>
+
+        <div className="product-name w-full h-[36px] pt-[12px]">
+            <p className="text-[#191C1D] leading-[24px]">
+                {product.title}
+            </p>
+        </div>
+
+        <div className="product-price h-[24px] w-full flex gap-[8px] items-center">
+            <p className="text-[#8A5100] font-bold leading-[24px]">
+                ₹{product.price}
+            </p>
+
+            <p className="text-[#554434] text-sm leading-[24px] line-through">
+                ₹{product.originalPrice}
+            </p>
+        </div>
+
+        <div className="add-to-card-button pt-[20px] pb-[8px] h-[52px] w-full flex justify-center items-center">
+            <a href="#!" className="text-[#0061A3] text-center leading-[24px]">
+                Add to Cart
+            </a>
+        </div>
+    </div>
+))}
                 </div>
             </div>
         </section>
